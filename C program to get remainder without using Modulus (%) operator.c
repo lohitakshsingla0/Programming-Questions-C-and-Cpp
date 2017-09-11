4ccd586f29b906064f6aba6d4d1f178c6ab3e103
lohitakshsingla0 <lohitakshsingla0@gmail.com>
2017-09-11 14:53:10 +0530

    c program

---
+++ C program to get remainder without using Modulus (%) operator.c
@@ -0,0 +1,18 @@
+/*
+	* Program to get remaindar without using % operator.
+*/
+
+#include <stdio.h>
+int main()
+{
+	int a,b,rem;
+	printf("Enter first number :");
+	scanf("%d",&a);
+	printf("Enter second number :");
+	scanf("%d",&b);
+
+	rem=a-(a/b)*b;
+
+	printf("Remainder is = %d\n",rem);
+	return 0;
+}
