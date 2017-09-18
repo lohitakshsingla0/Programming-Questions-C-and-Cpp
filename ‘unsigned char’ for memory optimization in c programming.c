6f1d7ac43a9a74184862d9764b5e5084e443a296
lohitakshsingla0 <lohitakshsingla0@gmail.com>
2017-09-18 15:23:09 +0530

    c program

---
+++ ‘unsigned char’ for memory optimization in c programming.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	unsigned char value=0;
+
+	printf("Value is: %d\n",value);
+
+	value=50;
+	printf("Value is: %d\n",value);
+
+	value=255;
+	printf("Value is: %d\n",value);
+
+	return 0;
+}
+
