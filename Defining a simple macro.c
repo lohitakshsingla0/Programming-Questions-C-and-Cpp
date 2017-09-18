e6399342e90cd1d47cdc12e5ac00c2e9a633a566
lohitakshsingla0 <lohitakshsingla0@gmail.com>
2017-09-18 15:36:11 +0530

    c program

---
+++ Defining a simple macro.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#define NAME    "includhelp.com"
+#define PI      3.14
+#define MAXBUFF 100
+
+int main()
+{
+    printf("\nNAME : %s",NAME);
+    printf("\nPI : %f",PI);
+    printf("\nMAXBUFF : %d",MAXBUFF);
+    return 0;
+}
