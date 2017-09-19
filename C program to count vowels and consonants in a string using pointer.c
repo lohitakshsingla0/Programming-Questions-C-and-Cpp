7085cff2194409bc16d2df6afaf844cd43470ba3
lohitakshsingla0 <lohitakshsingla0@gmail.com>
2017-09-19 11:21:04 +0530

    c programs

---
+++ C program to count vowels and consonants in a string using pointer.c
@@ -0,0 +1,28 @@
+/*C program to count vowels and consonants in a string using pointer.*/
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    char *ptr;
+    int  cntV,cntC;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    //assign address of str to ptr
+    ptr=str;
+
+    cntV=cntC=0;
+    while(*ptr!='\0')
+    {
+        if(*ptr=='A' ||*ptr=='E' ||*ptr=='I' ||*ptr=='O' ||*ptr=='U' ||*ptr=='a' ||*ptr=='e' ||*ptr=='i' ||*ptr=='o' ||*ptr=='u')
+            cntV++;
+        else
+            cntC++;
+        //increase the pointer, to point next character
+        ptr++;
+    }
+
+    printf("Total number of VOWELS: %d, CONSONANT: %d\n",cntV,cntC);
+    return 0;
+}
